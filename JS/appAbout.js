6b6a3d013f350d6951e8946e66a917439d3b2434
nismocoder <boostedrb240sx@gmail.com>
2021-03-15 19:42:19 +0700

    minor change to nav on about page

--- JS/appAbout.js
+++ JS/appAbout.js
@@ -9,35 +9,35 @@ tl.fromTo('.nav-container nav', {
     duration: 1
 }
 )
-// .fromTo('h2', {
-//     x: -900,
-//     opactity: 0,
-// },{
-//     x: 0,
-//     opactiy: 1,
-//     delay: 0,
-//     duration: .5,
-// }
-// )
-// .fromTo('h1', {
-//     x: -900,
-//     opactity: 0,
-// },{
-//     x: 0,
-//     opactiy: 1,
-//     delay: 0,
-//     duration: .5,
-// }
-// )
+.fromTo('h2', {
+    x: -900,
+    opactity: 0,
+},{
+    x: 0,
+    opactiy: 1,
+    delay: 0,
+    duration: .5,
+}
+)
+.fromTo('h1', {
+    x: -900,
+    opactity: 0,
+},{
+    x: 0,
+    opactiy: 1,
+    delay: 0,
+    duration: .5,
+}
+)
 
-// .fromTo('.h3', {
-//     opacity: 0,
-// },{
-//     opacity: 1,
-//     ease: "power2.inOut",
-//     duration: 1.2, x: 1, ease: "steps(5)",
-// }
-// )
+.fromTo('.h3', {
+    opacity: 0,
+},{
+    opacity: 1,
+    ease: "power2.inOut",
+    duration: 1.2, x: 1, ease: "steps(5)",
+}
+)
 
 .fromTo('.windowSeat', {
     opacity: 0,
